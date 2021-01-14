91eccdf4b7f220bc5a633260a7fc5390651bb98c
kitty-catt <ronald@oc8431623134.ibm.com>
2021-01-14 17:05:28 +0100

    add leading slash

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route('my_awesome_api', methods=['POST'])
+@app.route('/my_awesome_api', methods=['POST'])
 def my_awesome_endpoint():
     data = request.json
     return jsonify(data=data, meta={"status": "ok"})
