caef9c3bcef6f4b10af85d59dd9d2a9ceecf65c1
Kitty Catt <66872501+kitty-catt@users.noreply.github.com>
2023-09-23 12:06:44 +0200

    Update env.py

--- env.py
+++ env.py
@@ -1,5 +1,9 @@
 import os
  
 # printing environment variables
-for k, v in os.environ.items():
-    print(f'{k}={v}')
+# for k, v in os.environ.items():
+#    print(f'{k}={v}')
+
+name =os.environ['MY_NAME']
+surname = os.environ['MY_SURNAME']
+print(f'{name} home directory is {surname}')
